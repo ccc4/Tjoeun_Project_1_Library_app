7fab14c06118b79272aca504ff6ba673e13dc382
402-27 <402-27@DESKTOP-OH0SO1D>
2018-09-05 18:23:27 +0900

    ?

---
+++ Library_App/src/testPac/TT.java
@@ -0,0 +1,13 @@
+package testPac;
+
+import java.util.ArrayList;
+
+public class TT {
+	public static void main(String[] args) {
+		ArrayList<String> zzz = new ArrayList<>();
+		
+		zzz.add("ABC");
+		
+		System.out.println(zzz.get(0));
+	}
+}
